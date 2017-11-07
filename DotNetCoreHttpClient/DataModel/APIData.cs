9d2b34e1839b23d2f6a9c5260051a6a44e454541
李進興 Vulcan <vulcan@miniasp.com>
2017-11-07 18:17:21 +0800

    練習用到的資料模型 Data Model 類別

---
+++ DotNetCoreHttpClient/DataModel/APIData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataModel
+{
+    /// <summary>
+    /// 呼叫 API 的測試用類別
+    /// </summary>
+    public class APIData
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 檔案名稱
+        /// </summary>
+        public string Filename { get; set; }
+    }
+}
---
+++ DotNetCoreHttpClient/DataModel/LoginInformation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataModel
+{
+    /// <summary>
+    /// 模擬使用者登入用到的資料模型
+    /// </summary>
+    public class LoginInformation
+    {
+        /// <summary>
+        /// 帳號
+        /// </summary>
+        public string Account { get; set; }
+        /// <summary>
+        /// 密碼
+        /// </summary>
+        public string Password { get; set; }
+        /// <summary>
+        /// 驗證碼
+        /// </summary>
+        public string VerifyCode { get; set; }
+    }
+}
