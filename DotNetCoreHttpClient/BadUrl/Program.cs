1504c1dfaa9cbc01b2ab375c549c7a9027a931e8
李進興 Vulcan <vulcan@miniasp.com>
2017-11-07 19:48:42 +0800

    當指定的 URL 不存在的時候

---
+++ DotNetCoreHttpClient/BadUrl/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BadUrl
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            #region 將這段程式碼解除註解，了解非同步程式呼叫的例外異常處理方式
+            ////一般來說，若採用多執行緒運行，是無法捕捉到這些例外異常
+            //try
+            //{
+            //    HttpClient clientCapture = new HttpClient();
+            //    var fooResultCapture = await clientCapture.GetStringAsync("http://vulcanwebapi.azurewebsites.net/api/XXX/777");
+            //    Console.WriteLine($"{fooResultCapture}");
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine($"發現例外異常 {ex.Message}");
+            //}
+            #endregion
+
+            HttpClient client = new HttpClient();
+            var fooResult = await client.GetStringAsync("http://vulcanwebapi.azurewebsites.net/api/XXX/777");
+            Console.WriteLine($"{fooResult}");
+            Console.WriteLine($"Press any key to Exist...{Environment.NewLine}");
+            Console.ReadKey();
+        }
+    }
+}
